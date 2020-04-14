54a975c214de67f32c28550dd127ea0909e57a48
sprajjwal <shashwatprajjwal@gmail.com>
2020-04-13 18:48:37 -0700

    bucket sort done

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -1,11 +1,12 @@
 #!python
 
+from sorting_recursive import merge_sort
 
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
     then looping over counts and copying that many numbers into output list.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: O(max(n) + n) Why and under what conditions?
+    Memory usage: O(n) since we make a new array with all items."""
     if len(numbers) < 2:
         return
     # Find range of given numbers (minimum and maximum integer values)
@@ -19,8 +20,8 @@ def counting_sort(numbers):
     # Loop over given numbers and increment each number's count
     for num in numbers:
         array[num - minm] += 1
-    # TODO: Loop over counts and append that many numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    # Loop over counts and append that many numbers into output list
+    # Improve this to mutate input instead of creating new output list
     i, j = 0, 0
     while i < len(numbers):
         while array[j] < 1:
@@ -32,17 +33,39 @@ def counting_sort(numbers):
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum values)
-    # TODO: Create list of buckets to store numbers in subranges of input range
-    # TODO: Loop over given numbers and place each item in appropriate bucket
-    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
+    TODO: Running time: O(n.logn) since merge sort is called
+    TODO: Memory usage: O(n) since we make an extra 2d array for items"""
+    # Find range of given numbers (minimum and maximum values)
+    if len(numbers) < 2:
+        return
+    minm = min(numbers)
+    maxm = max(numbers)
+    bucket_range = (maxm - minm) // num_buckets + 1
+
+    # Create list of buckets to store numbers in subranges of input range
+    buckets = [[] for _ in range(num_buckets)]
+
+    # Loop over given numbers and place each item in appropriate bucket
+    for item in numbers:
+        index = (item - minm) // bucket_range
+        print('index: ', index)
+        buckets[index].append(item)
+
+    # Sort each bucket using any sorting algorithm (recursive or another)
+    for i in range(len(buckets)):
+        merge_sort(buckets[i])
+
     # TODO: Loop over buckets and append each bucket's numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    # Improve this to mutate input instead of creating new output list
+
+    i = 0
+    for bucket in buckets:
+        for item in bucket:
+            numbers[i] = item
+            i += 1
 
 
 if __name__ == "__main__":
-    num = [2, 2, 5, 3, 1]
-    counting_sort(num)
+    num = [2, 2, 0, 5, 3, 1]
+    bucket_sort(num)
     print(num)
--- Code/sorting_test.py
+++ Code/sorting_test.py
@@ -223,7 +223,7 @@ def get_sort_function():
 
 
 # If using PyTest, change this variable to the sort function you want to test
-sort = counting_sort
+sort = bucket_sort
 
 
 if __name__ == '__main__':
